5ac62c5b590c4c273528160e5ee42d054a8d89df
Armoghan <Armoghan16@gmail.com>
2023-11-18 23:10:02 +0500

    Added TailwindCss with Scss

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,19 @@
+/* The code is exporting an object that configures the Tailwind CSS framework. */
+
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./themes/**/layouts/**/*.html",
+    "./content/**/layouts/**/*.html",
+    "./layouts/**/*.html",
+    "./content/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ]
+}
